57ff1832405d4ad8d16adb1eb08bd4d1961337c8
dogabarsgan <doga.barsgan@richmond.edu>
2017-06-15 15:06:25 +0300

    Initial Commit

---
+++ styleUP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  styleUP
+//
+//  Created by Doğa Barsgan on 6/15/17.
+//  Copyright © 2017 Doğa Barsgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
