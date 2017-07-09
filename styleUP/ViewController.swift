57c0b483b28967637bfe990ce5af1a58967473a6
dogabarsgan <doga.barsgan@richmond.edu>
2017-07-09 15:31:38 +0300

    First version of the app that provides basic navigation, editing and viewing capabilities

--- styleUP/ViewController.swift
+++ styleUP/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Doğa Barsgan. All rights reserved.
 //
 
+
 import UIKit
 
 
@@ -17,13 +18,14 @@ struct ClothingItem {
     var color: String
     
 }
+
+
 //  Initializing the myWardrobe array
 var myWardrobe = [ClothingItem]()
 
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource,UITextFieldDelegate {
     
-
     
     //  variables that are used to add to the wardrobe
     var typeSelected: String = ""
@@ -33,37 +35,28 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     //  variables for the picker view
     var clothType = ["T-Shirt", "Pants",]
     var color = ["Black", "White",]
-
+    
     
     //  IB Outlets:
     
-  
-        //  Home Page
-        @IBOutlet weak var upperBodyImageView: UIImageView!
-        @IBOutlet weak var lowerBodyImageView: UIImageView!
-
-    
-        // Wardrobe Page
+    //  Home Page
+    @IBOutlet weak var upperBodyImageView: UIImageView!
+    @IBOutlet weak var lowerBodyImageView: UIImageView!
     
     
-        @IBOutlet weak var textBox1: UITextField!
-        @IBOutlet weak var textBox2: UITextField!
+    // Wardrobe Page
+    @IBOutlet weak var textBox1: UITextField!
+    @IBOutlet weak var textBox2: UITextField!
     
     
-        @IBOutlet weak var dropDown1: UIPickerView!
-        @IBOutlet weak var dropDown2: UIPickerView!
+    @IBOutlet weak var dropDown1: UIPickerView!
+    @IBOutlet weak var dropDown2: UIPickerView!
     
     
-
+    //-------------------------------------------------------------------------------------------------------------------------------
     
-    
-//-------------------------------------------------------------------------------------------------------------------------------
-    
-
     
     //  picker view functions
-    
-    
     
     
     //  function that determines the number of colums of pickerview
@@ -83,7 +76,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             countRows = self.clothType.count
             
         }
-        
+            
         else if (pickerView == dropDown2){
             
             countRows = self.color.count
@@ -95,7 +88,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     
-   
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
         if (pickerView == dropDown1){
@@ -105,7 +97,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             return titleRow
             
         }
-        
+            
         else if (pickerView == dropDown2){
             
             let titleRow = color[row]
@@ -115,19 +107,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
         
         return ""
-
+        
     }
+    
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
         
-
         if(pickerView == dropDown1){
             
             self.textBox1.text = self.clothType[row]
             
-
             typeSelected = self.clothType[row]
- 
+            
             self.dropDown1.isHidden = true
             
         }
@@ -137,16 +128,15 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             self.textBox2.text = self.color[row]
             
             colorSelected = self.color[row]
-           
+            
             self.dropDown2.isHidden = true
-
             
         }
         
     }
     
     
-     func textFieldDidBeginEditing(_ textField: UITextField) {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
         
         if( textField == self.textBox1){
             
@@ -154,7 +144,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             textField.resignFirstResponder()
             
             self.dropDown1.isHidden = false
-
             
         } else if ( textField == self.textBox2) {
             
@@ -162,12 +151,13 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             textField.resignFirstResponder()
             
             self.dropDown2.isHidden = false
-
+            
         }
         
     }
     
-//-------------------------------------------------------------------------------------------------------------------------------
+    
+    //-------------------------------------------------------------------------------------------------------------------------------
     
     
     // IB Outlets for buttons
@@ -188,7 +178,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     
-    
     @IBAction func addTapped(_ sender: Any) {
         
         print("pants Tapped")
@@ -196,12 +185,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     
-    
-    
     @IBAction func addToWardTapped(_ sender: Any) {
         
         //  add the cloth struct to the list of structs
-        
         
         let cloth = ClothingItem(type: typeSelected,color: colorSelected)
         
@@ -209,10 +195,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         print(myWardrobe.count)
         
-
+        
         
     }
-    
     
     
     //  button that prints the content of the wardrobe
@@ -233,54 +218,24 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBAction func randomize(_ sender: Any) {
         
-        
         //upperBodyImageView.image = UIImage(named: "tshirtBlue")
         
         //lowerBodyImageView.image = UIImage(named: "pantsBlack")
-
-        
-        
-        
-        
-        
         
     }
     
     
+    //-------------------------------------------------------------------------------------------------------------------------------
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-//-------------------------------------------------------------------------------------------------------------------------------
-
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         
         super.didReceiveMemoryWarning()
@@ -292,39 +247,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     //  hiding keyboard when outside of keyboard is touched
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        
         self.view.endEditing(true)
-        
         
     }
     
-    //  pressing return key to hide
     
+    //  pressing return key to hide
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         textField.resignFirstResponder()
         return(true)
     }
     
-//-------------------------------------------------------------------------------------------------------------------------------
-    
-    
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //-------------------------------------------------------------------------------------------------------------------------------
     
     
 }
-
